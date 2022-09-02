54d66ba8ca2816e705ce320abf85dc3cd618b895
Adailton Saraiva <s.adailton@gmail.com>
2022-09-02 01:06:16 -0300

    desenvolvimento exercicio 1c

--- Lista1/Exercicio 1a/main.py
+++ Lista1/Exercicio 1a/main.py
@@ -1,33 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from posixpath import split
+import os    
 
-def carrega_lista(arquivo):
+def soma_inteiros_posicao_imparos.path.abspath("Lista1/Exercicio 1b/lista_inteiros.txt")():
+
+    arquivo = 
+    lista_inteiros = []
+
     with open(arquivo, 'r', encoding='utf-8') as txtfile:
         f = txtfile.read()
         txtfile.close()
 
-        return f.split()
-
-def soma_inteiros_posicao_impar(lista_inteiros):
+        lista_inteiros = f.split()
 
     soma = 0
 
     # começa da posição 1 saltando de dois em dois
     for i in lista_inteiros[1::2]:
         soma = soma + int(i)
+
+    return str(soma)
     
-    return soma
 
-
-def main():
-
-    lista_de_inteiros = carrega_lista('lista_inteiros.txt')
-
-    soma = soma_inteiros_posicao_impar(lista_de_inteiros)
-    print(soma)
-    
-    return True
-
-if __name__ == '__main__':
-    main()
+soma_inteiros_posicao_impar()
--- Lista1/Exercicio 1c/main.py
+++ Lista1/Exercicio 1c/main.py
@@ -1,1 +1,49 @@
-from utils import rf
+from posixpath import split
+import os
+#from utils import rf
+
+programa = """
+def soma_inteiros_posicao_impar():
+
+    arquivo = os.path.abspath("Lista1/Exercicio 1b/lista_inteiros.txt")
+    lista_inteiros = []
+
+    with open(arquivo, 'r', encoding='utf-8') as txtfile:
+        f = txtfile.read()
+        txtfile.close()
+
+        lista_inteiros = f.split()
+
+    soma = 0
+
+    # começa da posição 2 saltando de três em três
+    for i in lista_inteiros[2::3]:
+        soma = soma + int(i)
+
+    return str(soma)
+    """
+programa = """
+def soma(a, b):
+    print('Adailton')
+
+soma
+"""
+def compara_lista():
+    
+    arquivo = os.path.abspath("Lista1/Exerciciob/main.py")
+    lista = []
+    with open(arquivo) as file_obj:
+        f = file_obj.read()
+        
+
+    # for linha in programa:
+    #     lista.append(eval(linha))
+
+    d = {'a': 2, 'b': 2}
+    
+    obj = compile(f,arquivo,mode='exec')
+
+    print(exec(obj, ))
+
+    isinstance()
+compara_lista()
